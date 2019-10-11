1d38ec9f651177783b71b9f76bac53ce948e6117
kacinskasS <53061402+kacinskasS@users.noreply.github.com>
2019-10-11 13:02:00 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <title>rookie tech internship</title>
+    <script src="script.js" defer></script>
+    <link rel="stylesheet" type="text/css" href="reset.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="utf-8">
+    <!--    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"-->
+    <!--          integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"-->
+    <!--          crossorigin="anonymous">-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+<div class="inner">
+    <div id="headerDiv" class="header">
+        <h2>To Do List App</h2>
+        <input type="text" id="listInput" placeholder="Write your task here...">
+        <span onclick="newElement()" class="addBtn">Add New Task</span>
+    </div>
+
+    <ul id="listUl">
+        <li>have some fun</li>
+
+    </ul>
+</div>
+</body>
+</html>
